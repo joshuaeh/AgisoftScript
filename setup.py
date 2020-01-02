69b705dcbd7a93f2cc9d7c3a92da849ca88b483d
Joshua E Hammond <jhammond02@gmail.com>
2020-01-01 22:21:35 -0700

    adds setup.py file for initializaiton

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Firefly',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='With permission from author',
+    author='Joshua Hammond',
+    author_email='joshuaeh@byu.edu',
+    description='An integrated workflow to create a flight plan from photos. From photos added in the Photobin file, firefly will create a .ply using the PhotoScan module, then using the Omega module to generate the flight path for the next iteration.'
+)
