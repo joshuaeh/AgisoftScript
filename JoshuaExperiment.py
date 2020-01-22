6c3eed550680d3e0de1a08f938351fc21de6d156
Joshua E Hammond <jhammond02@gmail.com>
2020-01-22 13:43:46 -0700

    adds omega generic structure

--- JoshuaExperiment.py
+++ JoshuaExperiment.py
@@ -5,7 +5,15 @@ import sys
 '''Instructions:
 
 
-
+To run on a computer with PhotoScan Pro:
+    Enter the following lines in the command line sequentially:
+        cd ../..
+        cd "Program Files\Agisoft\Photoscan Pro\photoscan.exe.exe" -r "Users\Civil\Desktop\firefly\[main].py"
+    As an alternative:
+        1) Open Agisoft
+        2) tools>run script
+        3) select the [main].py file (this file)
+        note: there will be no text output of feedback with this method
 
 '''
 
@@ -20,4 +28,4 @@ import sys
 # Omega Parameters
 
 
-os.popen('dir')
+
